cbf91aeb5ada5b771cd821080a65508da939ffab
soyeolesafm <soyelesafm@hotmail.com>
2021-07-26 21:36:44 -0500

    arrangin slides without css only js

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const rightButton = document.querySelector('.carousel__button--right')
 const indNav = document.querySelector('.carousel__nav')
 const indButtons = Array.from(indNav.children)
 
-
-
+// use getBoundingClientRect to obtain the data 
+// about width and other size and position messaurment 
+const slideWidth = slides[0].getBoundingClientRect().width
+// arrange slide position using only js
+slides[0].style.left = slideWidth * 0 + 'px'
+slides[1].style.left = slideWidth * 1 + 'px'
+slides[2].style.left = slideWidth * 2 + 'px'
 // ldebugger
-console.log(indButtons);
+console.log(slides[0]);
